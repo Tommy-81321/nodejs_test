633972c171300a631023e00591217301f34d0e63
Tommy-81321 <hchien13@my.bcit.ca>
2021-10-10 19:11:58 -0700

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var url = require('url');
+var PORT = process.env.PORT || 8080
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
   var txt = q.year + " " + q.month;
   res.end(txt);
-}).listen(8080);
+}).listen(PORT);
